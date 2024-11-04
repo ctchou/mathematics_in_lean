17f95b0016e8c5f25d0613e1a0b8ac8517a15e62
Ching-Tsun Chou <chingtsun.chou@gmail.com>
2024-11-04 14:57:08 -0800

    More work on MIL/C03_Logic/S01_Implication_and_the_Universal_Quantifier_ctchou.lean

--- MIL/C03_Logic/S01_Implication_and_the_Universal_Quantifier_ctchou.lean
+++ MIL/C03_Logic/S01_Implication_and_the_Universal_Quantifier_ctchou.lean
@@ -79,15 +79,32 @@ example (hfa : FnUb f a) (hgb : FnUb g b) : FnUb (fun x ↦ f x + g x) (a + b) :
   apply hfa
   apply hgb
 
-example (hfa : FnLb f a) (hgb : FnLb g b) : FnLb (fun x ↦ f x + g x) (a + b) :=
-  sorry
+example (hfa : FnLb f a) (hgb : FnLb g b) : FnLb (fun x ↦ f x + g x) (a + b) := by
+--  sorry
+  intro x
+  change a + b ≤ f x + g x
+  apply add_le_add
+  apply hfa
+  apply hgb
 
-example (nnf : FnLb f 0) (nng : FnLb g 0) : FnLb (fun x ↦ f x * g x) 0 :=
-  sorry
+example (nnf : FnLb f 0) (nng : FnLb g 0) : FnLb (fun x ↦ f x * g x) 0 := by
+--  sorry
+  intro x
+  simp
+  apply mul_nonneg
+  apply nnf
+  apply nng
 
 example (hfa : FnUb f a) (hgb : FnUb g b) (nng : FnLb g 0) (nna : 0 ≤ a) :
-    FnUb (fun x ↦ f x * g x) (a * b) :=
-  sorry
+    FnUb (fun x ↦ f x * g x) (a * b) := by
+--  sorry
+  intro x
+  simp
+  apply mul_le_mul
+  . apply hfa
+  . apply hgb
+  . apply nng
+  . apply nna
 
 end
 
