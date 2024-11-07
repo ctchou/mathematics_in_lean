870632ed7e45c1cab2ad8b9269a9fd2bbbc19fa5
Ching-Tsun Chou <chingtsun.chou@gmail.com>
2024-11-06 16:56:49 -0800

    Add MIL/C03_Logic/S05_Disjunction_ctchou.lean

--- MIL/C03_Logic/S05_Disjunction_ctchou.lean
+++ MIL/C03_Logic/S05_Disjunction_ctchou.lean
@@ -58,19 +58,57 @@ example : x < |y| → x < y ∨ x < -y := by
 namespace MyAbs
 
 theorem le_abs_self (x : ℝ) : x ≤ |x| := by
-  sorry
+--  sorry
+  rcases le_or_gt 0 x with h | h
+  . rw [abs_of_nonneg] ; exact h
+  . rw [abs_of_neg] ; linarith ; exact h
 
 theorem neg_le_abs_self (x : ℝ) : -x ≤ |x| := by
-  sorry
+--  sorry
+  rcases le_or_gt 0 x with h | h
+  . rw [abs_of_nonneg] ; linarith ; exact h
+  . rw [abs_of_neg] ; exact h
 
 theorem abs_add (x y : ℝ) : |x + y| ≤ |x| + |y| := by
-  sorry
+--  sorry
+  rcases le_or_gt 0 (x + y) with h | h
+  . rw [abs_of_nonneg h]
+    have hx := le_abs_self x
+    have hy := le_abs_self y
+    linarith
+  . rw [abs_of_neg h]
+    have hx := neg_le_abs_self x
+    have hy := neg_le_abs_self y
+    linarith
 
 theorem lt_abs : x < |y| ↔ x < y ∨ x < -y := by
-  sorry
+--  sorry
+  rcases le_or_gt 0 y with h | h
+  . rw [abs_of_nonneg h]
+    constructor
+    . tauto
+    . intro h1
+      rcases h1 with h' | h' <;> linarith
+  . rw [abs_of_neg h]
+    constructor
+    . tauto
+    . intro h1
+      rcases h1 with h' | h' <;> linarith
 
 theorem abs_lt : |x| < y ↔ -y < x ∧ x < y := by
-  sorry
+--  sorry
+  rcases le_or_gt 0 x with h | h
+  . rw [abs_of_nonneg h]
+    constructor
+    . intro h1
+      constructor <;> linarith
+    . tauto
+  . rw [abs_of_neg h]
+    constructor
+    . intro h1
+      constructor <;> linarith
+    . rintro ⟨h1, h2⟩
+      linarith
 
 end MyAbs
 
@@ -91,23 +129,61 @@ example {m n k : ℕ} (h : m ∣ n ∨ m ∣ k) : m ∣ n * k := by
     apply dvd_mul_right
 
 example {z : ℝ} (h : ∃ x y, z = x ^ 2 + y ^ 2 ∨ z = x ^ 2 + y ^ 2 + 1) : z ≥ 0 := by
-  sorry
+--  sorry
+  rcases h with ⟨x, y, h⟩
+  have hx : 0 ≤ x^2 := pow_two_nonneg x
+  have hy : 0 ≤ y^2 := pow_two_nonneg y
+  rcases h with h | h <;> linarith
 
 example {x : ℝ} (h : x ^ 2 = 1) : x = 1 ∨ x = -1 := by
-  sorry
+--  sorry
+  have h1 : (x - 1) * (x + 1) = 0 := by
+    calc (x - 1) * (x + 1) = x^2 - 1 := by ring
+         _ = 0 := by linarith
+  have h2 : x - 1 = 0 ∨ x + 1 = 0 := eq_zero_or_eq_zero_of_mul_eq_zero h1
+  rcases h2 with h3 | h3
+  . left ; linarith
+  . right ; linarith
 
 example {x y : ℝ} (h : x ^ 2 = y ^ 2) : x = y ∨ x = -y := by
-  sorry
+--  sorry
+  have h1 : (x - y) * (x + y) = 0 := by
+    calc (x - y) * (x + y) = x^2 - y^2 := by ring
+         _ = 0 := by linarith
+  have h2 : x - y = 0 ∨ x + y = 0 := eq_zero_or_eq_zero_of_mul_eq_zero h1
+  rcases h2 with h3 | h3
+  . left ; linarith
+  . right ; linarith
 
 section
 variable {R : Type*} [CommRing R] [IsDomain R]
 variable (x y : R)
+omit [IsDomain R]
+
+lemma aux : ∀ a b : R, a + b = 0 → a = -b := by
+  intro a b h
+  calc a = a + b + -b := by abel
+       _ = -b := by simp [h]
 
 example (h : x ^ 2 = 1) : x = 1 ∨ x = -1 := by
-  sorry
+--  sorry
+  have h1 : (x - 1) * (x + 1) = 0 := by
+    calc (x - 1) * (x + 1) = x^2 - 1 := by ring
+         _ = 0 := by {rw [h] ; ring }
+  have h2 : x - 1 = 0 ∨ x + 1 = 0 := eq_zero_or_eq_zero_of_mul_eq_zero h1
+  rcases h2 with h3 | h3
+  . left ; apply eq_of_sub_eq_zero h3
+  . right ; apply aux x 1 h3
 
 example (h : x ^ 2 = y ^ 2) : x = y ∨ x = -y := by
-  sorry
+--  sorry
+  have h1 : (x - y) * (x + y) = 0 := by
+    calc (x - y) * (x + y) = x^2 - y^2 := by ring
+         _ = 0 := by simp [h]
+  have h2 : x - y = 0 ∨ x + y = 0 := eq_zero_or_eq_zero_of_mul_eq_zero h1
+  rcases h2 with h3 | h3
+  . left ; apply eq_of_sub_eq_zero h3
+  . right ; apply aux x y h3
 
 end
 
@@ -124,5 +200,15 @@ example (P : Prop) : ¬¬P → P := by
   contradiction
 
 example (P Q : Prop) : P → Q ↔ ¬P ∨ Q := by
-  sorry
-
+--  sorry
+  constructor
+  . intro h
+    by_cases h' : P
+    . right ; apply h h'
+    . left ; exact h'
+  . intro h
+    rcases h with h' | h'
+    . by_contra! h''
+      apply h' h''.1
+    . intro h''
+      exact h'
