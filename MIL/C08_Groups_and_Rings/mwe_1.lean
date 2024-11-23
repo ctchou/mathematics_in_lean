01ea67c45863a5e02a757b09c2c11dde12c8b635
Ching-Tsun Chou <chingtsun.chou@gmail.com>
2024-11-22 16:26:32 -0800

    Work on MIL/C08_Groups_and_Rings/mwe_1.lean

--- MIL/C08_Groups_and_Rings/mwe_1.lean
+++ MIL/C08_Groups_and_Rings/mwe_1.lean
@@ -1,10 +1,10 @@
 
+import Mathlib.GroupTheory.QuotientGroup.Basic
+--set_option diagnostics true
+--set_option diagnostics.threshold 0
 variable {G : Type*} [Group G] {M : Subgroup G} [M.Normal]
+
 #check G ⧸ M
 #check G = G
 #check M = M
-#check G ⧸ M = G ⧸ M
-
-variable {G H : Type*}
-#check G
-#check G = G
+#check (G ⧸ M) = (G ⧸ M)
